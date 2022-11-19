3919dc124fd5e890379cae7a85b9440bf2403ea2
jjuhey <dallicjh@gmail.com>
2022-11-19 21:05:59 +0900

    feat: apply url

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN
 
 export const NOTION_TOKEN = process.env.REACT_APP_NOTION_TOKEN
 export const NOTION_DATABASE = process.env.REACT_APP_NOTION_DATABASE
-// export const NOTION_DATABASE_URL = 'https://api.notion.com/v1/databases'
-// export const NOTION_DOMAIN = (process.env.NODE_ENV === 'development') ? `https://cors-anywhere.herokuapp.com/${NOTION_DATABASE_URL}/${NOTION_DATABASE_ID}` : `${NOTION_DATABASE_URL}/${NOTION_DATABASE_ID}`
+export const NOTION_DATABASE_URL = 'https://api.notion.com/v1/databases'
+export const NOTION_BASE_URL = (process.env.NODE_ENV === 'development') ? `https://cors-anywhere.herokuapp.com/${NOTION_DATABASE_URL}` : `${NOTION_DATABASE_URL}`
--- src/pages/NotionPage.tsx
+++ src/pages/NotionPage.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
-import AuthLayout from '../components/AuthLayout';
 import { Client } from '@notionhq/client';
-import { NOTION_DATABASE, NOTION_TOKEN } from '../config';
 
-const notion = new Client({ auth: NOTION_TOKEN });
+import { NOTION_BASE_URL, NOTION_DATABASE, NOTION_TOKEN } from '../config';
+
+import AuthLayout from '../components/AuthLayout';
+
+const notion = new Client({ auth: NOTION_TOKEN, baseUrl: NOTION_BASE_URL });
 
 const NotionPage = () => {
   useEffect(() => {
@@ -20,10 +22,37 @@ const NotionPage = () => {
 async function getNotionData() {
   if (!NOTION_TOKEN || !NOTION_DATABASE) return []
 
-  const response = await notion.databases.retrieve({ database_id: NOTION_DATABASE });
-  console.log(response)
+  // const response = await notion.databases.retrieve({ database_id: NOTION_DATABASE });
 
-  return response
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'Notion-Version': '2022-06-28',
+      'content-type': 'application/json',
+      Authorization: `Bearer ${NOTION_TOKEN}`
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          "property": "Title",
+          "direction": "ascending"
+        }
+      ],
+      page_size: 100,
+    })
+  };
+  
+  try {
+    const response = await fetch(`${NOTION_BASE_URL}/${NOTION_DATABASE}/query`, options)
+    const data = await response.json()
+    console.log(data)
+  
+    return data
+  } catch(err) {
+    console.log(err)
+    throw err
+  }
 }
 
 export default NotionPage;
